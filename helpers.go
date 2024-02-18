f122a8f851e46830f2b52fb7fc4ea7185377e95e
Micah Asowata <andrewgary963@gmail.com>
2024-02-18 00:23:45 +0000

    refactor: improve body parsing error handling

---
+++ helpers.go
@@ -0,0 +1,109 @@
+package jason
+
+import (
+	"bytes"
+	"net/http"
+	"reflect"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type Err struct {
+	Code int    `json:"code"`
+	Msg  string `json:"msg"`
+}
+
+func (e *Err) Error() string {
+	return e.Msg
+}
+
+// isBodyJSON ensures the media type of the request is set to application/json
+func isBodyJSON(r *http.Request) bool {
+	contentTypeHeader := r.Header.Get(ContentType)
+	mediaType := strings.ToLower(strings.TrimSpace(strings.Split(contentTypeHeader, ";")[0]))
+	return mediaType == ContentTypeJSON
+}
+
+func isDstPointer(dst interface{}) bool {
+	return reflect.TypeOf(dst).Kind() == reflect.Ptr
+}
+
+func isBodyTooLarge(err error) bool {
+	return bytes.Contains([]byte(err.Error()), []byte(": http: request body too large"))
+}
+
+func isBadlyFormedJSON(err error) bool {
+	return bytes.Contains([]byte(err.Error()), []byte("readObjectStart:")) ||
+		bytes.HasPrefix([]byte(err.Error()), []byte("skipFourBytes: expect null")) ||
+		bytes.Contains([]byte(err.Error()), []byte(": readStringSlowPath: unexpected end of input"))
+}
+
+func containsImproperAssignment(err error) bool {
+	return bytes.Contains([]byte(err.Error()), []byte(": Read")) ||
+		bytes.HasPrefix([]byte(err.Error()), []byte("Read"))
+}
+
+func isEmpty(err error) bool {
+	return bytes.Equal([]byte(err.Error()), []byte("EOF"))
+}
+
+func findErrorLocation(input string) int {
+	if !strings.Contains(input, "#") {
+		return 0
+	}
+
+	if !strings.Contains(input, "byte") {
+		return 0
+	}
+
+	startIndex := strings.Index(input, "#") + 1
+	endIndex := strings.Index(input, "byte") - 1
+
+	cleanedValue := strings.TrimSpace(input[startIndex:endIndex])
+
+	pos, _ := strconv.Atoi(cleanedValue)
+
+	return pos
+}
+
+func isUnknownField(err error) bool {
+	return bytes.HasPrefix([]byte(err.Error()), []byte("ReadObject: found unknown field:"))
+}
+
+func getFieldName(input string) string {
+	pattern := `\{([^:]+):`
+
+	re := regexp.MustCompile(pattern)
+
+	match := re.FindStringSubmatch(input)
+
+	if len(match) < 2 {
+		return ""
+	}
+
+	return match[1]
+}
+
+func getStmt(input string) string {
+	pattern := `\|([^|]+)\|`
+	re := regexp.MustCompile(pattern)
+
+	endIndex := len(input)
+	if !strings.Contains(input, "bigger") {
+		return ""
+	}
+
+	startIndex := strings.Index(input, "bigger")
+	if startIndex == -1 {
+		return ""
+	}
+
+	match := re.FindStringSubmatch(input[startIndex:endIndex])
+
+	if len(match) < 2 {
+		return ""
+	}
+
+	return match[1]
+}
--- read.go
+++ read.go
@@ -1,12 +1,10 @@
 package jason
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
 	"net/http"
-	"strings"
 )
 
 const (
@@ -14,22 +12,16 @@ const (
 	ContentTypeJSON = "application/json"
 )
 
-var (
-	ErrInvalidContentType = errors.New("jason: content type is not application/json")
-)
-
-// isBodyJSON ensures the media type of the request is set to application/json
-func isBodyJSON(r *http.Request) bool {
-	contentTypeHeader := r.Header.Get(ContentType)
-	mediaType := strings.ToLower(strings.TrimSpace(strings.Split(contentTypeHeader, ";")[0]))
-	return mediaType == ContentTypeJSON
-}
-
 // Read parses the provided request body, triages possible errors and formats their message
 func (j *Jason) Read(w http.ResponseWriter, r *http.Request, dst interface{}) error {
 	ok := isBodyJSON(r)
 	if !ok {
-		return ErrInvalidContentType
+		return &Err{Code: http.StatusUnsupportedMediaType, Msg: "content type is not application/json"}
+	}
+
+	ok = isDstPointer(dst)
+	if !ok {
+		return &Err{Code: http.StatusInternalServerError, Msg: "destination is not a pointer"}
 	}
 
 	r.Body = http.MaxBytesReader(w, r.Body, j.maxBodySize)
@@ -42,45 +34,25 @@ func (j *Jason) Read(w http.ResponseWriter, r *http.Request, dst interface{}) er
 
 	err := dec.Decode(&dst)
 	if err != nil {
-		var syntaxError *json.SyntaxError
-		var unmarshalTypeError *json.UnmarshalTypeError
-		var invalidUnmarshalError *json.InvalidUnmarshalError
-		var maxBytesError *http.MaxBytesError
 		switch {
-
-		case errors.As(err, &syntaxError):
-			return fmt.Errorf("body contains badly-formed JSON (at character %d)", syntaxError.Offset)
-
-		case errors.Is(err, io.ErrUnexpectedEOF):
-			return errors.New("body contains badly-formed JSON")
-
-		case errors.As(err, &unmarshalTypeError):
-			if unmarshalTypeError.Field != "" {
-				return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
-			}
-			return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
-
-		case errors.Is(err, io.EOF):
-			return errors.New("body must not be empty")
-
-		case strings.Contains(err.Error(), "ReadObject: found"):
-			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field")
-			return fmt.Errorf("body contains unknown field %s", fieldName)
-
-		case errors.As(err, &maxBytesError):
-			return fmt.Errorf("body must not be larger than %d bytes", maxBytesError.Limit)
-
-		case errors.As(err, &invalidUnmarshalError):
-			panic(err)
-
+		case isEmpty(err):
+			return &Err{Code: http.StatusBadRequest, Msg: "request body must not be empty"}
+		case containsImproperAssignment(err):
+			return &Err{Code: http.StatusBadRequest, Msg: fmt.Sprintf("request body contains an invalid value for %s (at character %d)", getFieldName(getStmt(err.Error())), findErrorLocation(err.Error()))}
+		case isBadlyFormedJSON(err):
+			return &Err{Code: http.StatusBadRequest, Msg: fmt.Sprintf("request body contains badly formed JSON (at position %d)", findErrorLocation(err.Error()))}
+		case j.disallowUnknownFields && isUnknownField(err):
+			return &Err{Code: http.StatusBadRequest, Msg: fmt.Sprintf("request body contains unknown field %s", getFieldName(getStmt(err.Error())))}
+		case isBodyTooLarge(err):
+			return &Err{Code: http.StatusRequestEntityTooLarge, Msg: fmt.Sprintf("request body must not be larger than %d", j.maxBodySize)}
 		default:
-			return err
+			return &Err{Code: http.StatusInternalServerError, Msg: err.Error()}
 		}
 	}
 
 	err = dec.Decode(&struct{}{})
 	if !errors.Is(err, io.EOF) {
-		return errors.New("body must only contain a single JSON value")
+		return &Err{Code: http.StatusBadRequest, Msg: "request body must contain just one JSON object"}
 	}
 
 	return nil
