c4c55475e0cdfae9ced84a81c823efefb3f84ba7
Micah Asowata <andrewgary963@gmail.com>
2024-02-18 02:11:25 +0000

    test: ensure proper marshal error handling

--- writer_test.go
+++ writer_test.go
@@ -29,3 +29,23 @@ func TestWriteWithIndentation(t *testing.T) {
 
 	assert.Equal(t, ContentTypeJSON, w.Header().Get(ContentType))
 }
+
+func TestWriteWithoutIndentationError(t *testing.T) {
+	j, w, _ := arrangeTest(t, "")
+
+	chanErr := make(chan int)
+
+	err := j.Write(w, http.StatusOK, Envelope{"name": chanErr}, nil)
+	require.NotNil(t, err)
+}
+
+func TestWriteWithIndentationError(t *testing.T) {
+	_, w, _ := arrangeTest(t, "")
+
+	j := New(5, true, true)
+
+	chanErr := make(chan int)
+
+	err := j.Write(w, http.StatusOK, Envelope{"name": chanErr}, nil)
+	require.NotNil(t, err)
+}
