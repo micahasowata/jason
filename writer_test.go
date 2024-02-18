7891efc7d50fccb47cd8bbaeaf3336facad918a4
Micah Asowata <andrewgary963@gmail.com>
2024-02-18 02:02:25 +0000

    test: ensure writer proper behavior (w/o indent)

---
+++ writer_test.go
@@ -0,0 +1,20 @@
+package jason
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestWriteWithoutIndentation(t *testing.T) {
+	j, w, _ := arrangeTest(t, "")
+	header := make(http.Header)
+	header.Set("Connection", "Keep-Alive")
+
+	err := j.Write(w, http.StatusOK, Envelope{"name": "Jason"}, header)
+	require.Nil(t, err)
+
+	assert.Equal(t, ContentTypeJSON, w.Header().Get(ContentType))
+}
