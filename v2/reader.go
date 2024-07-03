ea8969076d7a2f8d644d9e1ae0660f5bf6efeef0
Micah Asowata <andrewgary963@gmail.com>
2024-07-03 20:35:29 +0100

    feat: add the `Read` method

---
+++ v2/reader.go
@@ -0,0 +1,87 @@
+package v2
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"reflect"
+	"strings"
+)
+
+func isDstPointer(dst any) bool {
+	return reflect.TypeOf(dst).Kind() == reflect.Ptr
+}
+
+func isBodyJSON(r *http.Request) bool {
+	header := r.Header.Get("Content-Type")
+	values := strings.Split(header, ";")
+	mediaType := strings.ToLower(strings.TrimSpace(values[0]))
+	return mediaType == ContentType
+}
+
+func getUnknownField(err error) string {
+	errormessageSlice := strings.Split(err.Error(), " ")
+	return errormessageSlice[len(errormessageSlice)-1]
+}
+
+func getTypeError(err error) string {
+	unmarshalErr, ok := err.(*json.UnmarshalTypeError)
+	if !ok {
+		return err.Error()
+	}
+
+	if len(unmarshalErr.Field) > 0 {
+		return fmt.Sprintf(`body contains invalid value type for "%s" at character %d`, unmarshalErr.Field, unmarshalErr.Offset)
+	}
+
+	return fmt.Sprintf("body contains invalid value type at character %d", unmarshalErr.Offset)
+}
+
+func (p *Parser) Read(w http.ResponseWriter, r *http.Request, dst any) error {
+	hasCorrectDst := isDstPointer(dst)
+	if !hasCorrectDst {
+		return &Err{Code: http.StatusInternalServerError, message: "destination is not a pointer"}
+	}
+
+	isJSON := isBodyJSON(r)
+	if !isJSON {
+		return &Err{Code: http.StatusUnsupportedMediaType, message: "request body content type is not application/json"}
+	}
+
+	r.Body = http.MaxBytesReader(w, r.Body, int64(p.Limit))
+
+	decoder := json.NewDecoder(r.Body)
+	decoder.DisallowUnknownFields()
+
+	err := decoder.Decode(dst)
+	if err != nil {
+		var syntaxError *json.SyntaxError
+		var unmarshalError *json.UnmarshalTypeError
+
+		switch {
+		case errors.Is(err, io.EOF):
+			return &Err{Code: http.StatusBadRequest, message: "body is empty"}
+		case strings.Contains(err.Error(), "http: request body too large"):
+			return &Err{Code: http.StatusRequestEntityTooLarge, message: "body too large"}
+		case strings.Contains(err.Error(), "json: unknown field"):
+			return &Err{Code: http.StatusBadRequest, message: fmt.Sprintf("contains unknown field %s", getUnknownField(err))}
+		case errors.As(err, &syntaxError):
+			return &Err{Code: http.StatusUnprocessableEntity, message: fmt.Sprintf("body contains invalid syntax at character %d", syntaxError.Offset)}
+		case errors.As(err, &unmarshalError):
+			return &Err{Code: http.StatusBadRequest, message: getTypeError(err)}
+		default:
+			return &Err{Code: http.StatusInternalServerError, message: err.Error()}
+		}
+	}
+
+	err = decoder.Decode(&struct{}{})
+	if err != nil {
+		if !errors.Is(err, io.EOF) {
+			return &Err{Code: http.StatusBadRequest, message: "body contains multiple json objects"}
+		}
+	}
+
+	return nil
+}
---
+++ v2/reader_test.go
@@ -0,0 +1,115 @@
+package v2
+
+import (
+	"testing"
+)
+
+func TestNonPointerDestination(t *testing.T) {
+	parser, w, r := setupTest(t, "")
+
+	err := parser.Read(w, r, map[string]any{})
+	if err == nil {
+		t.Fatal("destination checks not properly handled")
+	}
+}
+
+func TestInvalidContentType(t *testing.T) {
+	parser, w, r := setupTest(t, "")
+
+	r.Header.Set("Content-Type", "text/plain")
+
+	err := parser.Read(w, r, map[string]any{})
+	if err == nil {
+		t.Fatal("content type checks not properly handled")
+	}
+}
+
+func TestVeryLargeBody(t *testing.T) {
+	parser, w, r := setupTest(t, `{"name": "heyy"}`)
+	parser.Limit = 2
+	r.Header.Set("Content-Type", "application/json")
+
+	var input struct {
+		Name string `json:"name"`
+	}
+
+	err := parser.Read(w, r, &input)
+	if err == nil {
+		t.Fatal("body size limit checks not properly handled")
+	}
+}
+
+func TestRequestBodyErrors(t *testing.T) {
+	tests := []struct {
+		name string
+		body string
+		want string
+	}{
+		{
+			name: "empty body",
+			body: "{}",
+			want: "body is empty",
+		},
+		{
+			name: "has syntax error",
+			body: `<?xml version="1.0" encoding="UTF-8"?><note><to>Jason</to></note>`,
+			want: "body contains invalid syntax at character 1",
+		},
+		{
+			name: "has field with invalid value",
+			body: `{"name": 123}'`,
+			want: `body contains invalid value type for "name" at character 12`,
+		},
+		{
+			name: "has invalid structure",
+			body: `["foo", "bar"]`,
+			want: "body contains invalid value type at character 1",
+		},
+		{
+			name: "contains unknown field",
+			body: `{"email": "p@happy.com"}`,
+			want: `contains unknown field "email"`,
+		},
+		{
+			name: "has multiple objects",
+			body: `{"name":"Jason"}{"name":"Xoxo"}`,
+			want: "body contains multiple json objects",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			parser, w, r := setupTest(t, tt.body)
+			r.Header.Set("Content-Type", "application/json")
+
+			var input struct {
+				Name string `json:"name"`
+			}
+
+			err := parser.Read(w, r, &input)
+			if err != nil {
+				if err.Error() != tt.want {
+					t.Logf("wanted %s got %s", tt.want, err.Error())
+				}
+			}
+		})
+	}
+}
+
+func TestRead(t *testing.T) {
+	parser, w, r := setupTest(t, `{"name": "jason"}`)
+	r.Header.Set("Content-Type", "application/json")
+
+	var input struct {
+		Name string `json:"name"`
+	}
+
+	err := parser.Read(w, r, &input)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	if input.Name != "jason" {
+		t.Fatal("request not decoded")
+	}
+}
